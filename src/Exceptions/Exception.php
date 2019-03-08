2c0663a759bbc36a7e3ed0cdfffb0e516b8ddc7e
zh <1950796924@qq.com>
2019-03-08 11:34:34 +0800

    first commit

---
+++ src/Exceptions/Exception.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zh
+ * Date: 2019/3/8
+ * Time: 9:54
+ */
+
+namespace Laraveler\Weather\Exceptions;
+
+class Exception extends \Exception
+{
+
+}
---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zh
+ * Date: 2019/3/8
+ * Time: 9:56
+ */
+
+namespace Laraveler\Weather\Exceptions;
+
+class InvalidArgumentException extends Exception
+{
+
+}
